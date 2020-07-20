699ad6c11c331513c02c517615d8a236f3893eb5
shubham sharma <shubhamm8989@gmail.com>
2020-07-20 10:35:52 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './compnents/home/home.component';
+import { CategoryComponent } from './compnents/category/category.component';
+import { AboutComponent } from './compnents/about/about.component';
+import { ContactComponent } from './compnents/contact/contact.component';
+import { LoginComponent } from './compnents/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  { path: '',   redirectTo: 'home', pathMatch: 'full' },
+  {path: 'category', component: CategoryComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MDBBootstrapModule } from "angular-bootstrap-md";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './compnents/home/home.component';
+import { NavbarComponent } from './compnents/navbar/navbar.component';
+import { FooterComponent } from './compnents/footer/footer.component';
+import { ContactComponent } from './compnents/contact/contact.component';
+import { AboutComponent } from './compnents/about/about.component';
+import { CategoryComponent } from './compnents/category/category.component';
+import { LoginComponent } from './compnents/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContactComponent,
+    AboutComponent,
+    CategoryComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
